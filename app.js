3b87212902c349321ddd93aa29343fb939d99750
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-22 15:41:41 -0600

    Confingure dotenv

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const logger = require('morgan');
+
+dotenv.config({
+    path: '/config/config.env'
+})
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+app.use('/api/v1', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
